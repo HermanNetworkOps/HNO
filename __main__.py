0c35119614db4522f0ad357413b41a33a806b2c2
jherman19446 <jherman@localhost.localdomain>
2023-01-28 14:38:30 -0500

    too many changes

--- __main__.py
+++ __main__.py
@@ -2,30 +2,25 @@ import pulumi
 import pulumi_aws as aws
 from pulumi_aws import ec2
 subnets = 0
-config = pulumi.Config()
-# Create an AWS resource
-def subnet():
-	import pulumi 
-subnets = config.get_int("subnets" , 3)
+# Create an AWS resource 
 vpc = ec2.Vpc("vpc", cidr_block="10.0.0.0/16")
+priv_subnets = []
+pub_subnets = []
 for i in range(0, subnets):
-    subnets = ec2.Subnet(f"subnet-{i}",
-    vpc_id = vpc.id,    
-	cidr_block=f"10.0.{i + 1}.0/24")
+    priv_subnet = ec2.Subnet(f"subnet-{i}-priv",
+        vpc_id=vpc.id,
+        cidr_block=f"10.0.{i + 1}.0/24")
+    pub_subnet = ec2.Subnet(f"subnet-{i}-pub",
+        vpc_id=vpc.id,
+        cidr_block=f"10.0.{i + 11}.0/24")
+    priv_subnets.append(priv_subnet)
+    pub_subnets.append(pub_subnet)
 
-subnet()
 
 igw = aws.ec2.InternetGateway(
 	"HNO-igw",
 	vpc_id =vpc.id
 )
-subnet()
-
-ngw = aws.ec2.NatGateway
-"HNO-ngw"
-vpc_id = vpc.id
-
-
 route_table = aws.ec2.RouteTable(
 	"HNO-route-table-public",
 	vpc_id=vpc.id,
@@ -36,6 +31,16 @@ route_table = aws.ec2.RouteTable(
 		}
 	]
 )
+rt_assoc = aws.ec2.RouteTableAssociation(
+	"HNO-rta-public",
+	route_table_id= "HNO-route-table-public",
+	subnet_id = [subnet.id for subnet in pub_subnets] # do an operation on subnet, then the for loop for subnet in subnets
+)
+
+ngw = aws.ec2.NatGateway
+"HNO-ngw"
+vpc_id = vpc.id
+
 route_table = aws.ec2.RouteTable(
 	"HNO-route-table-private",
 	vpc_id=vpc.id,
@@ -47,11 +52,7 @@ route_table = aws.ec2.RouteTable(
 	]
 )
 
-rt_assoc = aws.ec2.RouteTableAssociation(
-	"HNO-rta",
-	route_table_id= "HNO-route-table-private",
-	subnet_id=subnet.id
-)
+
 sg = aws.ec2.SecurityGroup(
 	"HNO-http-sg",
 	description="Allow http traffic to ec2 instance",
@@ -61,7 +62,7 @@ sg = aws.ec2.SecurityGroup(
 		"to_port": 80,
 		"cidr_blocks": ["0.0.0.0/0"],
 	}],
-    vpc_id=vpc.id,
+    vpc_id= vpc.id,
 )
 ami = aws.ec2.get_ami(
 	most_recent="true",
@@ -81,7 +82,7 @@ ec2_instance = aws.ec2.Instance(
 	vpc_security_group_ids=[sg.id],
 	ami = ami.id,
 	user_data=user_data,
-      subnet_id=subnet.id,
+      subnet_id=pub_subnets.id,
       associate_public_ip_address=True,
 )
 
